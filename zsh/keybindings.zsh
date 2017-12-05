5f9ffbff9656100ad16bb10bd8c6de7f0ed8115b
Kees Kluskens <kees@webduck.nl>
2017-12-05 16:32:21 +0100

    Remove vim mode
    Fixes #5

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,35 +1,26 @@
-bindkey -v
-bindkey -M vicmd $'\e[2~' vi-insert
-bindkey -M vicmd $'\e[3~' delete-char
-bindkey -M vicmd _ beginning-of-line
-bindkey -M vicmd '^A' beginning-of-line
-bindkey -M vicmd $'\e[1~' beginning-of-line
-bindkey -M vicmd '^E' end-of-line
-bindkey -M vicmd $'\e[4~' end-of-line
-bindkey -M vicmd $'\e[5~' beep # pgup
-bindkey -M vicmd $'\e[6~' beep # pgdn
-bindkey -M vicmd $'\eOd' vi-backward-blank-word
-bindkey -M vicmd $'\eOc' vi-forward-blank-word
+bindkey "^[[H" beginning-of-line # home
+bindkey "^[[F" end-of-line # end
+bindkey $'\e[3~' delete-char
+bindkey _ beginning-of-line
+bindkey '^A' beginning-of-line
+bindkey $'\e[1~' beginning-of-line
+bindkey '^E' end-of-line
+bindkey $'\e[4~' end-of-line
+bindkey $'\e[5~' beep # pgup
+bindkey $'\e[6~' beep # pgdn
 
-bindkey -M viins $'\e[A' up-history
-bindkey -M viins $'\e[B' down-history
-bindkey -M viins '^R' history-incremental-search-backward
-bindkey -M viins ' ' magic-space
-bindkey -M viins $'\e[2~' overwrite-mode
-bindkey -M viins $'\e[3~' delete-char
-bindkey -M viins '^A' beginning-of-line
-bindkey -M viins $'\e[1~' beginning-of-line
-bindkey -M viins $'\e[7~' beginning-of-line
-bindkey -M viins $'\eOH' beginning-of-line
-bindkey -M viins '^E' end-of-line
-bindkey -M viins $'\e[4~' end-of-line
-bindkey -M viins $'\e[8~' end-of-line
-bindkey -M viins $'\eOF' end-of-line
-bindkey -M viins $'\e[5~' beep # pgup
-bindkey -M viins $'\e[6~' beep # pgdn
-bindkey -M viins '^?' backward-delete-char
-bindkey -M viins $'\eOd' vi-backward-blank-word
-bindkey -M viins $'\eOc' vi-forward-blank-word
-
-# Delay after switching to vim normal mode; default is 40
-export KEYTIMEOUT=1
+bindkey $'\e[A' up-history
+bindkey $'\e[B' down-history
+bindkey '^R' history-incremental-search-backward
+bindkey ' ' magic-space
+bindkey $'\e[2~' overwrite-mode
+bindkey $'\e[3~' delete-char
+bindkey '^A' beginning-of-line
+bindkey $'\e[7~' beginning-of-line
+bindkey $'\eOH' beginning-of-line
+bindkey '^E' end-of-line
+bindkey $'\e[8~' end-of-line
+bindkey $'\eOF' end-of-line
+bindkey $'\e[5~' beep # pgup
+bindkey $'\e[6~' beep # pgdn
+bindkey '^?' backward-delete-char
